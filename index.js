530e6172246477178f887d34381e4ddf541afeb0
Rahul <rsaini698@gmail.com>
2023-04-01 14:06:31 +0530

    connect to mongodb

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8000;
+const db = require('./config/mongoose');
+const expressLayout = require('express-ejs-layouts');
 
-app.get('/', (req, res)=>{
-    return res.send("HELLO");
-});
+// ejs engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+// layout
+app.use(expressLayout);
+// extract style and script from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+app.use('/', require('./routes'));
+
 
 app.listen(PORT, (err)=>{
     if(err){
